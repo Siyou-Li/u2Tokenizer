0ee2fa4ea937d42b242a188fc09924f2878cb603
Siyou-Li <ZMpurse@Gmail.com>
2025-12-30 23:50:15 +0000

    Remove deprecated training scripts and add new qwen3-1.7B training scripts

--- script/amos_mm_stage1.sh
+++
@@ -1,5 +0,0 @@
-sh /import/c4dm-04/siyoul/u2Tokenizer/script/amos_mm_stage1/amos_mm_diffts_stage1.sh
-sh /import/c4dm-04/siyoul/u2Tokenizer/script/amos_mm_stage1/amos_mm_dmtp_stage1.sh
-sh /import/c4dm-04/siyoul/u2Tokenizer/script/amos_mm_stage1/amos_mm_mu2_stage1.sh
-sh /import/c4dm-04/siyoul/u2Tokenizer/script/amos_mm_stage1/amos_mm_linvt_stage1.sh
-sh /import/c4dm-04/siyoul/u2Tokenizer/script/amos_mm_stage1/amos_mm_rpe_stage1.sh
