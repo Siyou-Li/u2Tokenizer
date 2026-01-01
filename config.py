a55af6a8940ccd9679d5d1367d26d4c472925047
Siyou-Li <ZMpurse@Gmail.com>
2026-01-01 10:01:48 +0000

    Refactor config.py: Simplify project path handling by removing base_dir variable

--- config.py
+++ config.py
@@ -7,13 +7,8 @@
 import json
 import pathlib
 
-
-base_dir = str(pathlib.Path(__file__).parent)
-
 """
 读取 json 配置文件。
 """
-f = open(f"{base_dir}/config/project.json", encoding='utf-8')
+f = open("config/project.json", encoding='utf-8')
 config = json.load(f)
-
-config["project_path"] = base_dir
